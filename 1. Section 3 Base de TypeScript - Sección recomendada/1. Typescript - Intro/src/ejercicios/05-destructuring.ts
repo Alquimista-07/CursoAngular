c701994f9bbf76cac07132b3c4bed9a652e174dc
Alquimista-07 <jdca.07@gmail.com>
2022-04-20 16:56:04 -0500

    20. Desestructuración de Arreglos

--- 1. Section 3 Base de TypeScript - Sección recomendada/1. Typescript - Intro/src/ejercicios/05-destructuring.ts
+++ 1. Section 3 Base de TypeScript - Sección recomendada/1. Typescript - Intro/src/ejercicios/05-destructuring.ts
@@ -45,3 +45,33 @@ console.log( 'El volumen actual es: ', vol );
 console.log( 'El segundo actual es: ', segundo );
 console.log( 'La canción actual es: ', cancion );
 console.log( 'El autor actual es: ', autor );
+
+
+//===========================================
+// 20. Desestructuración de Arreglos
+//===========================================
+
+const dbz:string[] = [ 'Goku', 'Vegeta', 'Trunks' ];
+
+const [ p1, p2, p3 ] = dbz;
+
+// Si necesitara solo una posición en especifico como en la destructuración de arreglos lo que importa es la posición
+// simplemente dejamos vacio
+const [ , , trunks ] = dbz;
+
+console.log( 'Personaje 1:', p1 );
+console.log( 'Personaje 2:', p2 );
+console.log( 'Personaje 3:', p3 );
+console.log( 'Personaje 3:', trunks );
+
+//---------------------------------------------------------------------------------------------------------------------------
+// NOTA: Adicionalmente si tuvieramos otro arreglo interno podríamos hacer otra destructuración sin problemas
+//       como por ejemplo si tuvieramos lo siguiente:
+//
+// const dbz:string[] = [ 'Goku', 'Vegeta', 'Trunks', [] ];
+//
+// const [ , , trunks:[] ] = dbz;
+//
+// Pero de cierto modo esto es raro que se presente y que llegue a necesitarse.
+//---------------------------------------------------------------------------------------------------------------------------
+
