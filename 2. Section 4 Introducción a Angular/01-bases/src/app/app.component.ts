1e480e304dd4ca1852b9e225f6372223507bf131
Alquimista-07 <jdca.07@gmail.com>
2022-04-24 09:01:16 -0500

    37. App Component y explicación sobre este

--- 2. Section 4 Introducción a Angular/01-bases/src/app/app.component.ts
+++ 2. Section 4 Introducción a Angular/01-bases/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+// Este decorador recibe al menos 2 argumentos que son el selector, y especificar si
+// por lo menos tiene una contraparte de html
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', // Nombre que este componente tiene
+  templateUrl: './app.component.html', // Contraparte de html
+  //-----------------------------------------------------------------------------------------------------------
+  // Existe otro template el cual nos permite definir ya sea un template literal 
+  // (parecido al templateUrl) o un string con lo que queremos mostrar por ejemplo:
+  //
+  // OJO en este caso al usar este template se comentaría y se ignoraría el templateURL 
+  // y se podría borrar
+  //
+  //template: '<span>Juan</span>',
+  //
+  // Otra forma de usarlo es la siguiente:
+  /*
+  template: `
+    <h1>Hola de nuevo</h1>
+    `,
+  */
+  //-----------------------------------------------------------------------------------------------------------
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App'; // Acá agregamos el tipado ya que es algo común
 }
