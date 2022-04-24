410e2d49c62506058c231b46018ddb07a22bf30b
Alquimista-07 <jdca.07@gmail.com>
2022-04-24 13:42:37 -0500

    42. Componente de Heroe y separación de directorios

--- 2. Section 4 Introducción a Angular/01-bases/src/app/app.module.ts
+++ 2. Section 4 Introducción a Angular/01-bases/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 
 // Importamos el componente ya que si no lo agregamos al module nos va a dar error
 // cuando tratemos de usarlo en el template del app.component
-import { contadorComponent } from './contador.component';
+import { contadorComponent } from './contador/contador.component';
+
+// Impoertamos el heroe component
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    contadorComponent
+    contadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
