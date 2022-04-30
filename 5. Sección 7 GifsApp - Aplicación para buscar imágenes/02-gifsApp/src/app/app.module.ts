7f4c1f4f151942d8c92dfb6e8b480d0bc89bba8b
Alquimista-07 <jdca.07@gmail.com>
2022-04-30 09:55:27 -0500

    80. GifsModule y sus componentes

--- 5. Sección 7 GifsApp - Aplicación para buscar imágenes/02-gifsApp/src/app/app.module.ts
+++ 5. Sección 7 GifsApp - Aplicación para buscar imágenes/02-gifsApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     // Agregamos el modulo share
-    SharedModule
+    SharedModule,
+    // Agegamos el modulo de gifs
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
