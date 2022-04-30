a8246bcf139cf75abd4bb6da82dbb05cbb674403
Alquimista-07 <jdca.07@gmail.com>
2022-04-30 11:04:48 -0500

    81. @ViewChild - Obtener referencias a objetos del HTML

--- 5. Sección 7 GifsApp - Aplicación para buscar imágenes/02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 5. Sección 7 GifsApp - Aplicación para buscar imágenes/02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+// Usamo el decorador ViewChild
+// Y le pasamos el elemento que pasamos por referencia local
+// Y con el cual vamos a ayudarnos par purgar la caja de texto luego de darle enter
+// NOTA: Acá el txtBuscar nos arroja un error porque nos dice que el elemento pueda que no exista
+//       pero como estoy seguro de que siempre va a existir usamos el operado Non-null-assertion operator (!)
+//       Para ver más documentación podemos visitar: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+@ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  // Función para buscar 
+  buscar() {
+
+    //console.log( this.txtBuscar );
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log( valor );
+
+    // Mandamos como vacío para que se limpie loa caja de texto
+    this.txtBuscar.nativeElement.value = '';    
+
   }
 
 }
