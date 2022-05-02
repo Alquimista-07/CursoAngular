2269d92e771142ce86fd7be413025979bff1d639
Alquimista-07 <jdca.07@gmail.com>
2022-05-02 11:27:45 -0500

    103. Componente Sidebar

--- 6. Sección 8 SPA - PaisesApp/03-paisesApp/src/app/shared/sidebar/sidebar.component.ts
+++ 6. Sección 8 SPA - PaisesApp/03-paisesApp/src/app/shared/sidebar/sidebar.component.ts
@@ -3,7 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  // En los estilos definimos una propidad para que solo los li del componente
+  // sidebar aparezcan con el cursor (manita) para que el usuario sepa que es 
+  // un link y que puede hacer click en ellos. Y esto se puede hacer ya que 
+  // Angular permite manejar el css de manera encapsulada por componente
   styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class SidebarComponent implements OnInit {
