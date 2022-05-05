529fbcad9f5f94c106ea199b31e81805d1920e55
Alquimista-07 <jdca.07@gmail.com>
2022-05-05 14:55:55 -0500

    136. Prime Button y estilo global

--- 7. Sección 10 Pipes de Angular/04-pipesApp/src/app/app.module.ts
+++ 7. Sección 10 Pipes de Angular/04-pipesApp/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// PrimeNg
+// Importamos el modulo de PrimeNg para usar los botones
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
