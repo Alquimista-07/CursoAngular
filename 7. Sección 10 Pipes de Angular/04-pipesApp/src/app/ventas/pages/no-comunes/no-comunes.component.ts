fcdc473eea4220923c242a06dc52f020b0ed211c
Alquimista-07 <jdca.07@gmail.com>
2022-05-08 14:15:59 -0500

    151. I18nPluralPipe

--- 7. Sección 10 Pipes de Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 7. Sección 10 Pipes de Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  // i18nSelect
   nombre: string = 'Ariadna';
 
   // Entonces para determinar el género y cambiar las palabras que contengan género
@@ -22,4 +23,20 @@ export class NoComunesComponent {
     'femenino' : 'invitarla'
   }
 
+  // i18nPlural
+  // Este pipe i18nPlural también recibe el argumento pluralMap el cual es obligatorio y el valor locale
+  // el cual es opcional.
+  clientes: string[] = [ 'Maria', 'Pedro', 'Juan', 'Ariadna', 'Fernando' ];
+
+  // Como necesitamos el mapa lo creamos y el cual es un objeto que tiene que tener las diferentes opciones
+  // que puede tener el valor clientes.length el cual trabaja con valores tanto positivos como negativos
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.', // Si el length es igual a cero entonces lo que se va a mostrar es el valor asignado
+    '=1': 'tenemos un cliente esperando.',        // Si el length es igual a uno entonces cambiamos el mensaje
+    '=2': 'tenemos 2 clientes esperando',         // Si el length es igual a 2 cambiamos el mensaje
+    // Ahora para no hacer todo esto de repetir a cada rato entonces tenemos un valor por defecto que
+    // es el other al cual le asignamos el signo numeral (#) y este se encarga ya de asignar automáticamente el valor que nos esta danado
+    // el length de la variable.
+    'other': 'tenemos # clientes esperando'
+  }
 }
