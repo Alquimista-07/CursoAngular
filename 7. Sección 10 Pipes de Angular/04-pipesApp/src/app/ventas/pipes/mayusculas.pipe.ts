fd3193d10ff022be4595f6ac3076a47eadf72e06
Alquimista-07 <jdca.07@gmail.com>
2022-05-10 16:00:02 -0500

    162. Valor y argumentos a los pipes personalizados

--- 7. Sección 10 Pipes de Angular/04-pipesApp/src/app/ventas/pipes/mayusculas.pipe.ts
+++ 7. Sección 10 Pipes de Angular/04-pipesApp/src/app/ventas/pipes/mayusculas.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class MayusculasPipe implements PipeTransform {
 
-    transform(value: string): string {
-        return 'Hola Mundo!!!...';
+    // Como queremos que el argumento enMayusculas sea ocional, entonces para hacerlo opcional
+    // le asignamos un valor por defecto
+    transform(value: string, enMayusculas: boolean = true): string {
+        // console.log( value );
+        // console.log( enMayusculas );
+
+        // if( enMayusculas === true ){
+        // La siguiente condición es la misma que la anterior
+        // if( enMayusculas ){
+        //     return value.toLocaleUpperCase();
+        // }else{
+        //     return value.toLocaleLowerCase();
+        // }
+
+        // Lo mismo que se hizo anteriormente se puede hacer usando un ternario
+        return ( enMayusculas ) 
+            ? value.toLocaleUpperCase() 
+            : value.toLocaleLowerCase();
     }
 
 }
