11d5f7511e7c1318959a89b0a34d833590afce70
Alquimista-07 <jdca.07@gmail.com>
2022-05-13 10:02:55 -0500

    185. Material Sidenav, Toolbar e iconos

--- 9. Sección 13 HeroesApp - Angular Material & Angular Flex-Layout/05-heroesApp/src/app/heroes/heroes.module.ts
+++ 9. Sección 13 HeroesApp - Angular Material & Angular Flex-Layout/05-heroesApp/src/app/heroes/heroes.module.ts
@@ -7,12 +7,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 // Importamos las rutas hijas para heroes
 import { HeroesRoutingModule } from './heroes-routing.module';
 
+// Importamos el modulo especializado de los componentes de Angular Material
+import { MaterialModule } from '../material/material.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-
 
 
 
@@ -27,6 +29,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
--- 9. Sección 13 HeroesApp - Angular Material & Angular Flex-Layout/05-heroesApp/src/app/material/material.module.ts
+++ 9. Sección 13 HeroesApp - Angular Material & Angular Flex-Layout/05-heroesApp/src/app/material/material.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 
+// Ahora si en este proyecto vamos a iniciar a trabajar con angular material ya que anteriormente solo se había 
+// instalado ejecutando el comando ng add @angular/material
+// Para ver más información sobre Angular Material podemos ir al enlace
+// https://material.angular.io/
+// Y para ver la documentación de los iconos podemos ir al enlace https://fonts.google.com/icons?selected=Material+Icons:bookmark
 
+// Entonces en este modulo vamos a importar todo lo correspondiente a Angular Material
+
+// Importamos los botones
+import { MatButtonModule } from '@angular/material/button';
+// Importamos los iconos
+import { MatIconModule } from '@angular/material/icon';
+// Importamos el sidenav
+import { MatSidenavModule } from '@angular/material/sidenav';
+// Importamos el toolbar
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   exports: [
-    
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
