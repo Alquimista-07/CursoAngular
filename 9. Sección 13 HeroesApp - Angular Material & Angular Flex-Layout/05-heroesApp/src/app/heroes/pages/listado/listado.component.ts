657e580f851f83608a45f8de897f7240c6b90ec1
Alquimista-07 <jdca.07@gmail.com>
2022-05-14 08:32:43 -0500

    191. Flex Layout - Diferentes resoluciones

--- 9. Sección 13 HeroesApp - Angular Material & Angular Flex-Layout/05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 9. Sección 13 HeroesApp - Angular Material & Angular Flex-Layout/05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -9,7 +9,11 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
+  styles: [`
+    mat-card {
+      margin-top: 20px;
+    }
+  `
   ]
 })
 export class ListadoComponent implements OnInit {
