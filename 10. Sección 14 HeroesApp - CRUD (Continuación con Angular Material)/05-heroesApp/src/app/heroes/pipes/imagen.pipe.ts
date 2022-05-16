a37696686b6e6f1cb4d630fc9db1b6b881ab2913
Alquimista-07 <jdca.07@gmail.com>
2022-05-16 09:55:06 -0500

    212. Pipes Puros e Impuros

--- 10. Sección 14 HeroesApp - CRUD (Continuación con Angular Material)/05-heroesApp/src/app/heroes/pipes/imagen.pipe.ts
+++ 10. Sección 14 HeroesApp - CRUD (Continuación con Angular Material)/05-heroesApp/src/app/heroes/pipes/imagen.pipe.ts
@@ -3,7 +3,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  // Acá existe un inconveniente con el pipe ya que este no se dispara cuando actualizamos la imágen
+  // y por lo tanto no se renderiza, entonces si nosotros queremos que suceda lo que suceda siempre
+  // se vuelva a disparar le asignamos el siguiente atributo.
+  // NOTA: Por lo tanto revisanso la documentación tenemos lo siguiente:
+  //       Cuando es verdadero, la canalización es pura, lo que significa que el método transform () 
+  //       se invoca solo cuando cambian sus argumentos de entrada. Las canalizaciones son puras de 
+  //       forma predeterminada.
+  //
+  //       Si la canalización tiene un estado interno (es decir, el resultado depende de un estado 
+  //       distinto de sus argumentos), configure pure como falso. En este caso, la canalización se 
+  //       invoca en cada ciclo de detección de cambios, incluso si los argumentos no han cambiado.
+  //
+  //       Y con base a lo anterior asignamos la propiedad pure en false
+  /*
+  pure: false
+  */
+ // Pero de momento no la vamos a dejar ya que consume muchos recursos ya que al pasar por cada parte
+ // del ciclo de vida de detección de cambios se va a disparar
 })
 export class ImagenPipe implements PipeTransform {
 
