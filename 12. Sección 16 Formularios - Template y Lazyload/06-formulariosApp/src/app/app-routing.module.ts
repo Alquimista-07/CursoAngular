26c835abd9d7fdd6e800fdcb5c2359b523a2012b
Alquimista-07 <jdca.07@gmail.com>
2022-05-17 17:49:08 -0500

    230. Inicio de la sección - Formularios

---
+++ 12. Sección 16 Formularios - Template y Lazyload/06-formulariosApp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// NOTA: Este archivo de rutas fue generado automáticamente con el Angular CLI cuando creamos la aplicación
+//       y no fue creado como veníamos haciendolo de forma manual en la cual creabamos el archivo, luego
+//       definiamos las rutas, importabamos y exportabamos los modulos para posteriormente importar este
+//       app-routing.module en el app.module
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
