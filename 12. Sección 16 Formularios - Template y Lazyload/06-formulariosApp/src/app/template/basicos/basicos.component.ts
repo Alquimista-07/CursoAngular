b81e84cb519ff09d5ebbb54f62f6483fe3fd5a26
Alquimista-07 <jdca.07@gmail.com>
2022-05-19 15:03:28 -0500

    240. Template: Limpiar el formulario

--- 12. Sección 16 Formularios - Template y Lazyload/06-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ 12. Sección 16 Formularios - Template y Lazyload/06-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -13,6 +13,12 @@ export class BasicosComponent implements OnInit {
   // y usar sus propiedades
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'RTX 4080Ti',
+    precio: 10,
+    existencias: 10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -38,6 +44,24 @@ export class BasicosComponent implements OnInit {
     // console.log( miFormulario.value );
     console.log( this.miFormulario.value );
     console.log( this.miFormulario );
+
+    // La siguiente linea de codigo me permite volver el formulario a su estado original
+    // es decir el pristine, el touche a su estado original y limpiar todos los campos.
+
+    // this.miFormulario.resetForm();
+
+    // Ahora también se le puede establecer un valor al campo, es decir si por ejemplo quiero que
+    // el campo existencias, precio y producto tengan el valor por defecto, entonces para ello le mandamos 
+    // un objeto como parametro al resetForm. Pero esto solo se establece hasta que se llama el metodo
+    // guardar entonces si queremos que se muestre esto desde que cargue el formulario entonces ocupamos
+    // hacerlo con una nueva propiedad que vamos a llamar initForm
+    this.miFormulario.resetForm({
+      producto: 'Sin Nombre',
+      precio: 0,
+      existencias: 0
+    });
+ 
+
   }
 
 }
