bb0c276954a8a11727ae4f1301bbc28368b0a2cf
Alquimista-07 <jdca.07@gmail.com>
2022-05-20 11:03:05 -0500

    251. Primeros pasos en formularios reactivos

--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+
+  // La idea de los formularios reactivos es que la parte del html se mantenga lo más limpia
+  // posible y las validaciones y lógica se encuentren en la parte del componente TypeScript
+
+  miFormulario: FormGroup = new FormGroup({
+    'producto': new FormControl( 'RTX 4080Ti' )
+  });
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/reactive.module.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/reactive.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Importamos el modulo de formularios reactivos
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
@@ -17,6 +19,8 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
+    // Lo primero para usar los formularios reactivos es importar el modulo de formularios reactivos
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
