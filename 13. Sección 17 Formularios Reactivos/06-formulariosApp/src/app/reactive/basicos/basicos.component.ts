d81f0837b519480788b178698a77d9128b6c043f
Alquimista-07 <jdca.07@gmail.com>
2022-05-20 14:19:02 -0500

    252. FormBuilder

--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -11,12 +11,32 @@ export class BasicosComponent {
 
   // La idea de los formularios reactivos es que la parte del html se mantenga lo más limpia
   // posible y las validaciones y lógica se encuentren en la parte del componente TypeScript
-
+  // Entonces como vamos a usar el formbuilder no ocupamos usar el formcontrol
+  /*
   miFormulario: FormGroup = new FormGroup({
-    'producto': new FormControl( 'RTX 4080Ti' )
+    'producto'   : new FormControl( 'RTX 4080Ti' ),
+    'precio'     : new FormControl( 1500 ),
+    'existencias': new FormControl( 5 )
+  });
+  */
+
+  // Para trajar con el FormBuilder necesitmos crear la propiedad
+  miFormulario: FormGroup = this.fb.group({
+    // Y aparartir de acá es trabajar con el objeto literal
+    // y debemos mandarlo en un arreglo porque deentro van las validaciones
+    // y las validaciones asíncronas. Adicionalmente se le puede establecer
+    // un valor por defecto
+    producto: [ 'RTX 4080Ti' ],
+    precio: [ 0 ],
+    existencias: [ 0 ]
   });
 
-  constructor() { }
+
+  // El FormBuilder nos ayuda a crear formularios bastante complejos o muy grandes pero
+  // que tenga la anotación de un objeto literal de JavaScript.
+  // Entonces el FormBuilder es un servicio por lo tanto necesitamos inyectarlo
+  constructor( private fb: FormBuilder ) { }
+
 
 
 }
