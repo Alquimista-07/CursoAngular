72469048913ee115bc57a27281b47c4856949dbf
Alquimista-07 <jdca.07@gmail.com>
2022-05-21 10:08:59 -0500

    256. Tarea: Validar un nuevo campo

--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  // Definimos el objeto par ael formulario y validomos
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [ Validators.required, Validators.minLength(3) ] ]
+  });
 
-  ngOnInit(): void {
+  // Inyectamos el servicio
+  constructor( private fb: FormBuilder ) { }
+
+  // Validamos para mostrar el menaje
+  campoEsValido ( campo: string ) {
+    return this.miFormulario.controls[ campo ].errors &&
+           this.miFormulario.controls[ campo ].touched;
+  }
+
+  guardar() {
+
+    if( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.miFormulario.value );
+    this.miFormulario.reset();
+
   }
 
 }
