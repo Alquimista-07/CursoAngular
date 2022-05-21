126ac14fa91e506d8758a22771bdf63a55719043
Alquimista-07 <jdca.07@gmail.com>
2022-05-21 13:23:49 -0500

    257. FormArray

--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,8 +11,22 @@ export class DinamicosComponent {
 
   // Definimos el objeto par ael formulario y validomos
   miFormulario: FormGroup = this.fb.group({
-    nombre: [ '', [ Validators.required, Validators.minLength(3) ] ]
+    nombre: [ '', [ Validators.required, Validators.minLength(3) ] ],
+    // Entonces inicialmente definimos a través del form builder un array que va a contener un arreglo
+    favoritos: this.fb.array( [
+      // Posteriormente vamos acá adentro tenemos colecciones de form control que se pueden definir simplemente al colocar []
+      [ 'Metal Gear', Validators.required ],
+      [ 'Call of Dutty', Validators.required ]
+    ], Validators.required )
   });
+
+  get favoritosArr() {
+    // Existe otra forma de tomar un control sin tener que hacer this.miFormulario.controls['favoritos'] el cual es el 
+    // método get que se muestra a continuación.
+    // NOTA: Al colocar el as FormArrya le estamos ayudando a TypesCruip a que entienda que el elemento es un FormArray
+    //       y no un control
+    return this.miFormulario.get( 'favoritos' ) as FormArray;
+  }
 
   // Inyectamos el servicio
   constructor( private fb: FormBuilder ) { }
