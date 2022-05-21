24956903217a9e9210c24fac2f7c47612a88fa16
Alquimista-07 <jdca.07@gmail.com>
2022-05-21 16:25:51 -0500

    260. Formularios Reactivos: Switches

--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/switches/switches.component.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  // Definimos el formulario
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    condiciones: [ false, Validators.requiredTrue ] // Acá usamos otro metodo del validatos que es el requiredTrue para que siempre tenga que estar en true para que el formulario sea válido
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  // Inyectamos el FormBuilder
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    // Expandimos el arreglo de persona y lo inicializamos
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    });
   }
+
 
 }
