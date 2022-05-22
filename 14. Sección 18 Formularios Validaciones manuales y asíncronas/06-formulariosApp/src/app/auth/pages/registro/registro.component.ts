c2c0906d8612150f1f6a92ceee67d14aa4aa4cd1
Alquimista-07 <jdca.07@gmail.com>
2022-05-22 13:30:49 -0500

    269. Validar contra una expresión regular

--- 14. Sección 18 Formularios Validaciones manuales y asíncronas/06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 14. Sección 18 Formularios Validaciones manuales y asíncronas/06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  //TODO: Temporal
+  // Creamos la expresción regular donde el preimer () valida que es para el nombre y el otro () es para el apellido
+  // La expresión valida que puede contener cualquier caracter de la a a la z múscula y de la A a la Z mayúscula y 
+  // luego el + indica cualquier cantidad de caracteres
+  regexNombreApellido: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  // Creamos la definición del formulario
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [ Validators.required, Validators.pattern( this.regexNombreApellido ) ] ]
+  });
+
+  // Inyectamos el FormBuilder
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  campoNoValido( campo: string ) {
+    return this.miFormulario.get(campo)?.invalid
+           && this.miFormulario.controls[campo]?.touched;
+  }
+
+  submitFormulario() {
+    console.log( this.miFormulario.value );
+    this.miFormulario.markAllAsTouched();
   }
 
 }
