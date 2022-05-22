7bef50b0e9f2627dca6616fd9d20372986499fee
Alquimista-07 <jdca.07@gmail.com>
2022-05-22 09:20:52 -0500

    261. Actualizar el valor de la persona

--- 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/switches/switches.component.ts
+++ 13. Sección 17 Formularios Reactivos/06-formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -30,6 +30,34 @@ export class SwitchesComponent implements OnInit {
       ...this.persona,
       condiciones: false
     });
+
+    // Ahora si queremos que los cambios se den en tiempo real podemos suscribirnos
+    // a los cambios del formuarlio usando rxjs
+    this.miFormulario.valueChanges.subscribe( ({ condiciones, ...restoDeArgumentos }) => {
+      // Usando la desestructuración separamos para tomar solo la información que me interesa
+      this.persona = restoDeArgumentos;
+    });
+
+    // También podemos suscribirnos a un campo en particular
+    this.miFormulario.get('condiciones')?.valueChanges.subscribe( newValue => {
+      console.log( newValue );
+    });
+
+  }
+
+  guardar() {
+
+    const formValue = {...this.miFormulario.value};
+
+    // Ahora para poder actualizar la persona con la información del formulario eliminamos la propiedad codiciones
+    delete formValue.condiciones;
+
+    // Luego de eliminar la propiedad ahora si la podemos asignar a la persona
+    this.persona = formValue;
+
+    console.log( formValue );
+
+
   }
 
 
