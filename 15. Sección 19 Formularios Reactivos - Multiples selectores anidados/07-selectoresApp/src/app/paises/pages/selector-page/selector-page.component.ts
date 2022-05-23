3b86cd49483e53d477477f9f1665abb758c06ac0
Alquimista-07 <jdca.07@gmail.com>
2022-05-23 17:39:39 -0500

    285. Formulario reactivo - primer selector

--- 15. Sección 19 Formularios Reactivos - Multiples selectores anidados/07-selectoresApp/src/app/paises/pages/selector-page/selector-page.component.ts
+++ 15. Sección 19 Formularios Reactivos - Multiples selectores anidados/07-selectoresApp/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  // Creamos el formulario y le mandamos como primer parámetro un string vacío, una primera validación síncrona de required
+  // como segundo parámetro y de momento no hay validaciones asíncronas
+  miFormulario: FormGroup = this.fb.group({
+    region: [ '', [ Validators.required ] ]
+  });
+
+  // Inyectamos el servicio FormBuilder
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  // Creamos el método que sera llamado cuando hagamos el submit
+  guardar() {
+    console.log( this.miFormulario.value );
   }
 
 }
