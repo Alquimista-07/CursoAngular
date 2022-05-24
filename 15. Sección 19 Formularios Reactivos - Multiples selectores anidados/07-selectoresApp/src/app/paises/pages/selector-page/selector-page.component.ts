783c3634d9e2d7f7697b6c5e50500cab64cabb75
Alquimista-07 <jdca.07@gmail.com>
2022-05-24 08:59:27 -0500

    287. Segundo selector anidado

--- 15. Sección 19 Formularios Reactivos - Multiples selectores anidados/07-selectoresApp/src/app/paises/pages/selector-page/selector-page.component.ts
+++ 15. Sección 19 Formularios Reactivos - Multiples selectores anidados/07-selectoresApp/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
+import { PaiseSmall } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -14,11 +15,13 @@ export class SelectorPageComponent implements OnInit {
   // Creamos el formulario y le mandamos como primer parámetro un string vacío, una primera validación síncrona de required
   // como segundo parámetro y de momento no hay validaciones asíncronas
   miFormulario: FormGroup = this.fb.group({
-    region: [ '', [ Validators.required ] ]
+    region: [ '', [ Validators.required ] ],
+    pais: [ '', [ Validators.required ] ]
   });
 
   // Llenar selectores
   regiones: string[] = [];
+  paises: PaiseSmall[] = [];
 
   // Inyectamos el servicio FormBuilder
   constructor( private fb: FormBuilder,
@@ -28,6 +31,20 @@ export class SelectorPageComponent implements OnInit {
     // Usualmente cuando queremos traer data de una API para inicializar normalmente
     // se hace en el ngOnInit
     this.regiones = this.paisesService.regiones;
+
+    // Cuando cambie la región
+    this.miFormulario.get('region')?.valueChanges
+        .subscribe(
+          region => {
+            console.log(region);
+            this.paisesService.getPaisesPorRegion( region )
+                .subscribe( paises =>{
+                  console.log( paises );
+                  this.paises = paises;
+                })
+          }
+        )
+
   }
 
   // Creamos el método que sera llamado cuando hagamos el submit
--- 15. Sección 19 Formularios Reactivos - Multiples selectores anidados/07-selectoresApp/src/app/paises/services/paises.service.ts
+++ 15. Sección 19 Formularios Reactivos - Multiples selectores anidados/07-selectoresApp/src/app/paises/services/paises.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { PaiseSmall } from '../interfaces/paises.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
+
+  private _baseUrl: string = environment.baseUrl;
 
   // Para evitar errores ya que se pasan por referencia por eso lo hacemos privado
   private _regiones: string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania' ];
@@ -15,5 +21,16 @@ export class PaisesService {
     return [...this._regiones];
   }
 
-  constructor() { }
+  // Inyectamos el servicio para las peticiones http
+  constructor( private http: HttpClient ) { }
+
+  // Creamos un servicio que lea la región y traiga los valores que devuelve el endpoint
+  getPaisesPorRegion( region: string ): Observable<PaiseSmall[]> {
+    
+    const url = `${this._baseUrl}/region/${region}?fields=cca3,name`;
+    
+    return this.http.get<PaiseSmall[]>( url );
+
+  }
+
 }
