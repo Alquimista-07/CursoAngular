f56fb7be087829ddf9d98e9f0ee51356a634afdf
Alquimista-07 <jdca.07@gmail.com>
2022-05-25 14:40:00 -0500

    299. ngOnChanges

--- 16. Sección 20 LifeCycle Hooks/08-lifecycleApp/src/app/components/muestra-nombre/muestra-nombre.component.ts
+++ 16. Sección 20 LifeCycle Hooks/08-lifecycleApp/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-muestra-nombre',
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class MuestraNombreComponent implements OnInit {
+export class MuestraNombreComponent implements OnInit, OnChanges {
+
+  @Input() nombre!: string;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // NOTA: Este ngOnCganges no se va a disparar o no va a ser llamado por el framework, si el componente no tiene 
+    //       inputs o si funciona sin proveer inputs. Y en cuantoa a esto input no se refiere a un input en el html
+    //       sino que se refiere a un input de un componente padre al hijo, es decir, cuando usamos un decorador @Input
+    console.log( changes );
+  }
 
   ngOnInit(): void {
   }
