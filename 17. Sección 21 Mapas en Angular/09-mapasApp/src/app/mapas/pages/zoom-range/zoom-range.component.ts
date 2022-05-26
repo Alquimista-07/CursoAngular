dd8fc84e013c6d20f9f00f251183fc480dc85966
Alquimista-07 <jdca.07@gmail.com>
2022-05-26 10:13:33 -0500

    311. Página Zoom-Range - Diseño y tarea

--- 17. Sección 21 Mapas en Angular/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 17. Sección 21 Mapas en Angular/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importamos el mapbox
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
+  styles: [`
+  .mapa-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  .row {
+    background-color: white;
+    border-radius: 5px;
+    bottom: 50px;
+    left: 50px;
+    padding: 10px;
+    position: fixed;
+    z-index: 999;
+  }
+
+  `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +30,14 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa-zoom-range',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -72.56019992138917, 6.318082682981575 ], 
+      zoom: 18
+    });
+
   }
 
 }
