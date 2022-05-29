b9190ae79e6638336cd0fbf075361e54edb4b19a
Alquimista-07 <jdca.07@gmail.com>
2022-05-29 15:33:04 -0500

    334. Gráfica de barra

--- 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/graficas.module.ts
+++ 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/graficas.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Importamos el ng2-chart en el modulo que lo vamos a usar
+import { NgChartsModule } from 'ng2-charts';
+
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
 import { DonaComponent } from './pages/dona/dona.component';
 import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+
 
 
 @NgModule({
@@ -19,7 +23,8 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraficasModule { }
