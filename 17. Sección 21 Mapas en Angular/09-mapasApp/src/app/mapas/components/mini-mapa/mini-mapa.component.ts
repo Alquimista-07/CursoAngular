e380d426c2e852fef4c91cef46d37e70db8d3285
Alquimista-07 <jdca.07@gmail.com>
2022-05-29 09:29:51 -0500

    325. Componente MiniMapa

--- 17. Sección 21 Mapas en Angular/09-mapasApp/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ 17. Sección 21 Mapas en Angular/09-mapasApp/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+// Importamos mapbox
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styles: [
+  styles: [`
+    div {
+      width: 100%;
+      height: 150px;
+      margin: 0px;
+    }
+  `
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  // Creamos un input para recibir la longitud y la latitud
+  @Input() lngLat: [ number, number] = [ 0, 0 ]; 
+
+  // Tomamos la referencia al div del miniMapa.component.html
+  @ViewChild('mapa') divMapa!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      // Quitamos la propiedad interactive para que el usuario no pueda arrastrar y moverse dentro de ellos
+      interactive: false
+    });
+
+    // Le cereamos el marcador al mapa
+    new mapboxgl.Marker({
+      draggable: false
+    })
+    .setLngLat( this.lngLat )
+    .addTo( mapa );
+    
   }
 
 }
