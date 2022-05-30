a5ca6a9ea2d95497565fc796b7dc912d2a6f7486
Alquimista-07 <jdca.07@gmail.com>
2022-05-30 14:29:52 -0500

    339. Gráfica de Dona mediante petición Http

--- 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/services/graficas.service.ts
+++ 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/services/graficas.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
 
-  constructor() { }
+  // Inyectamos el http para poderlo usar
+  constructor( private http: HttpClient ) { }
+  
+  baseUrl = environment.baseUrl;
+
+  // Creamos un método praa traer la información
+  getUsuariosRedesSociales() {
+    const url = `${this.baseUrl}/grafica`;
+    return this.http.get(url);
+  }
+
 }
