c87b3f9a896a8ed3c3a7ec580accaadf2b861ba7
Alquimista-07 <jdca.07@gmail.com>
2022-05-30 11:40:00 -0500

    337. Añadir flexibilidad a nuestro componente personalizado

--- 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -8,32 +8,29 @@ import { BaseChartDirective } from 'ng2-charts';
   styles: [
   ]
 })
-export class GraficaBarraComponent implements OnInit {
+export class GraficaBarraComponent {
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
-  public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true
+ 
+  @Input() barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      }
+    }
   };
-  public barChartType: ChartType = 'bar';
-
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
-    // NOTA: Para cambiar el color a la gráfica agregamos otro atributo que es el backgroundColor
-    //       adicionalmente una paleta de colores que nos puede ayudar la tenemos disponible en el
-    //       siguiente enlace: https://color.adobe.com/es/create/color-wheel
-    //       Adicionalmente existe otro parámetro que es el hoverBackgroundColor el cual define el color
-    //       que aparece cuando nos paramos sobre cada una de las barras de la gráfica.
+ 
+  @Input() barChartType: ChartType = 'bar';
+ 
+  @Input() barChartData: ChartData = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#638CF8', hoverBackgroundColor: 'yellow' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#579DD9', hoverBackgroundColor: 'yellow' },
-      { data: [ 8, 33, 40, 66, 86, 45, 100 ], label: 'Series C', backgroundColor: '#6DD5F0', hoverBackgroundColor: 'yellow' }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
+      { data: [ 82, 28, 1, 39, 46, 67, 50 ], label: 'Series C' }
+      
     ]
   };
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
