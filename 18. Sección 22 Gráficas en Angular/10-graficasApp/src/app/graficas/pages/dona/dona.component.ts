7f6ae7a76fbc8a8a74140b6b7e6f3d7cc24557bf
Alquimista-07 <jdca.07@gmail.com>
2022-05-30 13:15:31 -0500

    338. Gráfica de Dona

--- 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/pages/dona/dona.component.ts
+++ 18. Sección 22 Gráficas en Angular/10-graficasApp/src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  public colors: Color[] = [
+    '#BC76F5',
+    '#825FDE',
+    '#FABA53'
+  ];
 
-  ngOnInit(): void {
-  }
+  public colorsHover: Color[] = [
+    '#69A9FA',
+    '#5DB8DE',
+    '#5DDEBE'
+  ]
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+
+    // NOTA: Podemos usar la paleta de colores disponible en el siguiente enlace: https://color.adobe.com/es/create/color-wheel
+    //       Y para cambiar el color a la gráfica tenemos que mandar un arreglo de colores a la propiedad backgroundColor,
+    //       adicionalmente podemos cambiar el color cuando nos paremos con el mouse uando la propiedad hoverBackgroundColor
+    //       y mandandole nuevamente un arreglo de colores
+
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ], backgroundColor: this.colors, hoverBackgroundColor: this.colorsHover }
+    ]
+  };
+
+  public doughnutChartType: ChartType = 'doughnut';
+
+
 
 }
