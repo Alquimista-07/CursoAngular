a46022267d623143bda243328876ef6650fb0825
Alquimista-07 <jdca.07@gmail.com>
2022-05-31 15:52:09 -0500

    350. Cambiar el mensaje de la etiqueta

--- 19. Sección 23 Directivas personalizadas de Angular/11-directivasApp/src/app/shared/directives/error-msg.directive.ts
+++ 19. Sección 23 Directivas personalizadas de Angular/11-directivasApp/src/app/shared/directives/error-msg.directive.ts
@@ -16,6 +16,9 @@ export class ErrorMsgDirective implements OnInit {
   // para asignarla como un input y poder trabajar con ella recibiendo valores desde el elemento padre.
   @Input() color: string = 'red';
 
+  // Creamos una propiedad para recibir el mensaje
+  @Input() mensaje: string = 'Este campo es requerido';
+
   constructor( private elem: ElementRef<HTMLElement> ) { 
     console.log( 'Constructor directive' );
     // console.log( elem );
@@ -29,6 +32,9 @@ export class ErrorMsgDirective implements OnInit {
     console.log( 'NgOnInit directiva' );
     // Llamamos la función para cambiar el color al span
     this.setColor();
+
+    // Llamamos la función para cambiar el mensaje al span
+    this.setMensaje();
   }
 
   // Para modificar el color al span solamente bastaría con hacer lo siguiente
@@ -36,6 +42,20 @@ export class ErrorMsgDirective implements OnInit {
     // Acá hacemos referencia al elemento y le mandamos por el valor del Input que estamos recibiendo para obtener 
     // el color que el usuario haya querido usar o recibir el color rojo por defecto que se había asignado.
     this._htmlElement.nativeElement.style.color = this.color;
+    
+    // Adicionalmente podemos establecerle la clase al elemento html para que del lado de nuestro componente agregar.component.html
+    // quede más simplificado, para hacer esto podemos crear un nuevo método o simplemente aletar la propiedad directamente acá ya 
+    // que el metodo setColor ya lo estamos usando  entonces para hacer esto hacemos lo siguiente
+    this._htmlElement.nativeElement.classList.add( 'form-text' );
+    // NOTA: Adicionalmente del lado del componente agregar.component.html deje el color, el mensaje y elimine la clase del spoan para fines ilustrativos
+    //       pero como se menciónó estos ya no son necesarios y el span simplemente podría quedar con la directiva y el *ngIf.
+  
+  }
+
+  // Creamos el método para establecer el mensaje que se va a recibir desde el componente padre, o el valor que se tiene por defecto
+  // en caso de que no se envíe.
+  setMensaje(): void {
+    this._htmlElement.nativeElement.innerText = this.mensaje;
   }
 
 
