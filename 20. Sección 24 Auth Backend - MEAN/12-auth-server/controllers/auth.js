68a2bed7e961276f4de8e270c78b89999d7b4023
Alquimista-07 <jdca.07@gmail.com>
2022-06-04 12:32:33 -0500

    375. Crear usuario en base de datos

--- 20. Sección 24 Auth Backend - MEAN/12-auth-server/controllers/auth.js
+++ 20. Sección 24 Auth Backend - MEAN/12-auth-server/controllers/auth.js
@@ -2,8 +2,8 @@
 // como ayuda ya que si no la hacemos tendríamos un inconveniente el cual es que no tendríamos la ayuda del tipado
 const { response } = require('express');
 
-// Importamos el validarionRestult del express-validator
-const { validationResult } = require('express-validator');
+// Importamos el modelo
+const Usuario = require('../models/Usuario');
 
 // Como se había mencionado cada uno de los callback o controladores en nuestro archivo de rutas auth.js pueden crecer bastante
 // por lo tanto se recomienda separar el controlador del manejador de la ruta con el fin de mantenerlo lo más ordenado posible, 
@@ -17,7 +17,7 @@ const { validationResult } = require('express-validator');
 //================================
 // Crear un nuevo usuario
 //================================
-const crearUsuario = (req, res = response)=>{
+const crearUsuario = async(req, res = response)=>{
 
     //NOTA: Entonces para probar lo que estamos mandando por el body usamos postman, el cual en los en los endpoint de
     //      tipo POST (crearUsuario, loginUsuario) configuramos en el postman el body y le indicamos ras y luego el formato 
@@ -26,18 +26,62 @@ const crearUsuario = (req, res = response)=>{
     //      se debe investigar ya que en el curso no lo explican.
 
     // Y al darle enviar en el postman podemos hacer un console.log para ver como llega la información
-    console.log( req.body );
+    // console.log( req.body );
     
     // Entonces por lo tanto ya podemos extraer los datos que me intersan podemos desestructurar
     const { email, name, password } = req.body;
-    console.log( email );
-    console.log( name );
-    console.log( password );
+    // console.log( email );
+    // console.log( name );
+    // console.log( password );
 
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
+    try {
+
+        // 1. Verificar si no existe un email igual ya que el email lo definimos como único.
+        //    Para ello buscamos algo cuyo email sea igual al email que estoy recibiendo 
+        //    usando una función que ya trae mongoose que es el findOne() y le pasamos el objeto
+        //    { email : email } o simplemente como estamos usando el estándar de ES6 solo indicamos
+        //    la propiedad ya que ambas variables se llaman igual y ya ES6 lo infiere por nosotros
+        const usuario = await Usuario.findOne({ email });
+
+        // Lo anterior va a regresar un objeto y si encuentra un objeto que exista con dicho email
+        // entonces la variable usuario va a tener un valor y si tiene un valor tengo que indicarle 
+        // que no puedo crear el usuario porque ya el email esta registrado
+        if( usuario ){
+            // Hacemos el return con un bad request para que se salga y no continue
+            return res.status(400).json({
+                ok: false,
+                msg: 'El email ya existe'
+            });
+        }
+
+        // 2. Crear usuario con el modelo
+        const dbUser = new Usuario( req.body );
+        
+        // 3. Hashear (encriptar) la constraseña
+        
+        // 4. Generar el JSON Web Token (JWT) el cual se le va a enviar a Angular para que lo use
+        //    como un méotodo de autenticación pasiva
+
+        // 5. Insertar el usuario en la base de datos
+        await dbUser.save();
+        
+        // 6. Generar respuesta exitosa.
+        //    Le colocamos un status 201 que indica que se creo un nuevo registro
+        //    y cabe resaltar que cualquier status en el rango de 200 es exitoso
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name
+        });
+
+    }
+    catch (error) {
+        console.log( error );
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 
 }
 
