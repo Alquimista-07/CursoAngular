f986658ff7a8e17c335cc4785c9eebec34d4d441
Alquimista-07 <jdca.07@gmail.com>
2022-06-09 18:04:34 -0500

    419. Debounce Manual

--- 23. Sección 27 Bonus Mapas - Marcadores y Direcciones con Mapbox/mapas-app/src/app/maps/components/search-bar/search-bar.component.ts
+++ 23. Sección 27 Bonus Mapas - Marcadores y Direcciones con Mapbox/mapas-app/src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+
+  // Creamos una propiedad que es el timer que voy a esperar hasta que se cumpla y ahí 
+  // emitir el valor.
+  // NOTA: Al agregar el NodeJS.Timeout nos muestra un error en el navegador, entonces para solucionarlo debemos ir al archivo
+  //       tsconfig.app.json y en la parte de typs agregar node como se ve a continuación
+  /*
+            "types": [
+                "node"
+              ] 
+  */
+  private deboundeTimer?: NodeJS.Timeout;
 
   constructor() { }
+  
+  // Acá quiero controlar la manera como voy a emitir los valores
+  onQueryChanged( query: string = '' ){
+    
+    // Si se tiene un valor lo limpia.
+    // Explicación: Cada vez que nosotros estemos escribiendo en la caja de texto la instruccion del if se va a ejecutar
+    //              y lo empieza a limpiar cada vez, es decir, se establece un nuevo debounceTimer, se vuelve a establecer
+    //              pero nuevamente el siguiente viene y lo limpia y solo el último es el que se va a mantener. Despúes de
+    //              que pasen 350 milesimas de segundo de tiempo sin que se escriba nada en la caja de texto se va a mandar 
+    //              emitir el query
+    if ( this.deboundeTimer ) clearTimeout( this.deboundeTimer );
 
-  ngOnInit(): void {
+    this.deboundeTimer = setTimeout( () => {
+
+      console.log('Mandar este query: ', query);
+
+    }, 350);
+
   }
 
 }
